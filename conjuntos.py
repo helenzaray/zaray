67787aa3cea642ca9b4b90c6896f7a93de7a8548
Santiago <santiago.riveram@udea.edu.co>
2025-02-05 12:51:04 -0500

    first commit

---
+++ conjuntos.py
@@ -0,0 +1,26 @@
+""""
+conjunto = {" carro","casa","beca","empresa"}
+conjunto2 = set((" carro","casa","beca","empresa"))
+print(conjunto)
+print(conjunto2)
+"""
+
+frutas = {"manzana","piña","naranja","mandarina"}
+carros = {"toyota","fiat","ferrary","fiat", "piña"}
+
+
+print(frutas)
+print(carros)
+
+frutas.add("guanabanaa")
+print(frutas)
+
+copia = frutas.copy()
+print(copia)
+
+print(frutas.difference(carros))
+frutas.discard("guanabana")
+print(frutas)
+frutas.discard("piña")
+print(frutas.pop())
+frutas.intersection(carros)
---
+++ repaso.py
@@ -0,0 +1,12 @@
+usuarios = []
+
+while True:
+
+ nombre = input("digita un nombre o x"  )
+
+ if nombre == "X":
+    break
+
+usuarios.append(nombre)
+
+print("usuarios resistrados", usuarios)
